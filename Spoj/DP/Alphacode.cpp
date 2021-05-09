9cc21d61b7700e33676a04b1987bf4d08f65d472
chauhankartik <kartikchauhan987@gmail.com>
2021-05-09 21:51:52 +0530

    SPOJ/DP

--- Spoj/DP/Alphacode.cpp
+++ Spoj/DP/Alphacode.cpp
@@ -2,45 +2,32 @@
 using namespace std;
 #define int long long int
 
+int dp[5005];
+string str;
+
+int solve(int pos, int len)
+{       
+
+        if(pos == len) return 1;
+        if(str[pos] == '0') return 0;
+
+        if(dp[pos] != -1) return dp[pos];
+
+        dp[pos] = solve(pos + 1, len);
+        if(pos + 1 < len)
+        {
+                if(str[pos] == '1') dp[pos] += solve(pos+2, len);
+                else if(str[pos] == '2' && str[pos+1]<='6') dp[pos] += solve(pos+2, len);
+        }
+
+        return dp[pos];
+}
+
 int32_t main() {
 
-        int n;
-        while(scanf("%lld", &n) == 1 && n != 0) {
-                vector<int> dp(to_string(n).length()+1, 0);
-                string s = to_string(n);
-                dp[0] = 1;
-
-                for(int i = 1; i < s.length(); i++) {
-                        if(s[i] == '0' && s[i-1] == '0') {
-                                dp[i] = -1;
-                                break;
-                        }
-                        else if(s[i] == '0' && (s[i-1] == '1' || s[i-1] == '2'))
-                                dp[i] += 1;
-                        else if(s[i] == '0' && (s[i-1] != '1' && s[i-2] != '2')) {
-                                dp[i] = -1;
-                                break;
-                        }
-                        else if(s[i] != 0) {
-                                if(s[i-1] == '1') {
-                                        dp[i] += 1;
-                                }
-                                else if(s[i-1] == '2' && (s[i] - '0' <= 6)) {
-                                        dp[i] += 1;
-                                }      
-                        }
-                }
-                int res = 0;
-                bool flag = false;
-                for(int i = 0; i < dp.size(); i++) {
-                        if(dp[i] == -1) {
-                                cout << 0 << endl;
-                                flag = true;
-                        }
-                        else res += dp[i];
-                }
-                if(!flag)
-                        cout << res << endl;
+        while(cin >> str && str.compare("0")) {
+                memset(dp, -1, sizeof dp);
+                printf("%lld\n", solve(0, str.length()));
         }
         return 0;
 }
