8e449d0a0f9593ecb2ca4e6e6b4a8cd6a49d7e8a
chauhankartik <kartikchauhan987@gmail.com>
2021-05-10 16:49:02 +0530

    Leetcode/google

--- LeetCode/929_ UniqueEmailAddresses.cpp
+++ LeetCode/929_ UniqueEmailAddresses.cpp
@@ -1,3 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int numUniqueEmails(vector<string>& emails) {
@@ -8,10 +11,13 @@ public:
             bool seen_plus = false;
             string new_str = "";
             for(int j = 0; j < s.length(); j++) {
-                if(s[j] == '@')
-                    seen_at =true;
-                else if(s[j] == '+')
+                if(s[j] == '+' && seen_at == false){
                     seen_plus = true;
+                }
+                else if(s[j] == '@') {
+                    seen_at = true;
+                    new_str += s[j];
+                }
                 else if(seen_at) {
                     new_str += s[j];
                 }
@@ -28,3 +34,14 @@ public:
         return final.size();
     }
 };
+
+int main() {
+        vector<string> emails;
+        string s;
+        while(cin >> s) {
+                emails.push_back(s);
+        }
+        Solution obj;
+        cout << obj.numUniqueEmails(emails) << endl;
+        return 0;
+}
