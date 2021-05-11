313270b914942059e6b1266aeed2a2dee20715c1
chauhankartik <kartikchauhan987@gmail.com>
2021-05-12 00:07:55 +0530

    Leetcode/Google

---
+++ LeetCode/123_BestTimetoBuyandSellStockIII.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+        dp[100005];
+
+*/
+
+class Solution {
+        int dp[100005][2][5]; // dp[days][own][k]
+public:
+        int helper(vector<int>& prices,
+                   int          pos   ,
+                   int          own   ,
+                   int          k     )
+        {
+                int n = prices.size()-1;
+                for(int days = n-1; days >= 0; days--){
+                        for(int trans = 0; trans <= k; trans++) {
+                                if(days == n-1) {
+                                                // sell -- base cases
+                                                dp[days][1][0] = 0;
+                                                dp[days][1][trans] = (trans >= 1) ? prices[days] : 0;
+                                }
+                                else {
+                                        dp[days][0][trans] = max(((trans >= 1) ? -prices[days] + dp[days+1][1][trans] : 0),dp[days+1][0][trans]); 
+                                        dp[days][1][trans] = max((trans >= 1) ? prices[days] + dp[days+1][0][trans-1] : 0, dp[days+1][1][trans]);
+                                }
+                        }
+                }
+                return dp[0][0][k];
+        }
+
+        int maxProfit(vector<int>& prices) 
+        {
+                memset(dp, 0, sizeof dp);
+                return helper(prices, 0, 0, 2);
+        }
+};
+
+int main() {
+        Solution obj;
+        vector<int> p; int n;
+        while (cin>>n)
+        {
+                p.push_back(n);
+        }
+        cout << obj.maxProfit(p) << endl;
+        return 0;
+}
