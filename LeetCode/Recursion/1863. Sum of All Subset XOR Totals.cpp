663a9ce9ae967ebeb2b8809043d3eb1302ec8d95
chauhankartik <kartikchauhan987@gmail.com>
2021-05-24 03:51:53 +0530

    BackTracking

--- LeetCode/Recursion/1863. Sum of All Subset XOR Totals.cpp
+++ LeetCode/Recursion/1863. Sum of All Subset XOR Totals.cpp
@@ -4,8 +4,26 @@ using namespace std;
 class Solution {
 public:
         int res = 0;
+        int ans  = 0;
+
+        void helper(vector<int>& nums, int i) {
+                if(i >= nums.size()) {
+                        ans += res;
+                        return;
+                }
+
+                // take nums[i]
+                res ^= nums[i];
+                helper(nums, i+1);
+
+                // don't take nums[i]
+                res ^= nums[i];
+                helper(nums, i+1);
+
+        }
 
         int subsetXORSum(vector<int>& nums) {
-                        
-        }
+                helper(nums , 0);
+                return ans;
+        }       
 };
