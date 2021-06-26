ef69a517d514836fbb155be84ae9eb3d10edf101
chauhankartik <kartikchauhan987@gmail.com>
2021-06-26 19:55:56 +0530

    IntegentBinarySearch

--- LeetCode/BinarySearch/33. Search in Rotated Sorted Array.cpp
+++ LeetCode/BinarySearch/33. Search in Rotated Sorted Array.cpp
@@ -4,11 +4,27 @@ using namespace std;
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int rotated_idx = -1;
         int low = 0, high = nums.size() - 1;
+        // find pivot
+        while(low < high) {
+            int mid = low + (high - low) / 2;
+            if(nums[mid] > nums[high]) low = mid + 1;
+            else high = mid;
+        }
+        int root = low;
+        low = 0;high = nums.size() - 1;
+
+        if(target >= nums[root] && target <= nums[high])
+            low = root;
+        else 
+            high = root;
+
         while(low <= high) {
             int mid = low + (high - low) / 2;
-            
+            if(target == nums[mid]) return mid;
+            if(target > nums[mid]) low = mid + 1;
+            else high = mid - 1;
         }
+        return -1;
     }
 };
