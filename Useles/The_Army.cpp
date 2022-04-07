a590c5ba5d603f148ae0faf916fd1c5c1dce72bc
Kartik <kartik.chauhan@rapipay.com>
2022-04-07 22:26:42 +0530

    git push origin master

---
+++ Useles/The_Army.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(0), cin.tie(0);
+    int t; cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        vector<int> vec(n, 0);
+        for(int i = 0, pos; i < m; i++) {
+            cin >> pos;
+            
+        }
+    }
+    
+    return 0;
+}
